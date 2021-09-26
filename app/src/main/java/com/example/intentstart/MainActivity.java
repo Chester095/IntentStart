d1b9572d479b32afc2f3eb392c1e558fec950678
Chester095 <Chester095@users.noreply.github.com>
2021-09-26 20:14:17 +0300

    Lesson 5 Task 3: start тоже косячит

--- app/src/main/java/com/example/intentstart/MainActivity.java
+++ app/src/main/java/com/example/intentstart/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.intentstart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -10,9 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TEXT = "PARAM";
+    private final static String number = "PARAM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Uri uri = Uri.parse("example://intent");
                 Intent runEchoIntent = new Intent(Intent.ACTION_VIEW, uri);
-                runEchoIntent.putExtra(TEXT, text.getText().toString());
-                ActivityInfo activityInfo =runEchoIntent.resolveActivityInfo(getPackageManager(),
-                                runEchoIntent.getFlags());
+                runEchoIntent.putExtra(number, text.getText().toString());
+                ActivityInfo activityInfo = runEchoIntent.resolveActivityInfo(getPackageManager(),
+                        runEchoIntent.getFlags());
                 if (activityInfo != null) {
                     startActivity(runEchoIntent);
                 }
